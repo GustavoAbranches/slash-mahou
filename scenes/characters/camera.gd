c87e8df64cf97fc37f1eb0b27e54a1160860da27
Gustavo Abranches <gusabranches@gmail.com>
2025-10-07 12:39:55 -0300

    Concerta os sprites de animação e configura as colisões dos inimigos

---
+++ scenes/characters/camera.gd
@@ -0,0 +1,10 @@
+class_name Camera
+
+extends Character
+
+@export var player : Player
+
+func handle_input() -> void: 
+	if player != null:
+		var direction := (player.position - position).normalized() 
+		velocity = direction * speed
